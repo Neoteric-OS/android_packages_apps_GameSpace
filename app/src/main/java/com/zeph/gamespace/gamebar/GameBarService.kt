pe-gm/packages_apps_GameSpace

GameSpace: Long click Settings icon to open Settings Dashboard
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
Signed-off-by: hydrangea07 <104114625+hydrangea07@users.noreply.github.com>

app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBarService.kt
@@ -40,6 +40,7 @@ import com.android.systemui.screenrecord.IRecordingCallback
 import dagger.hilt.android.AndroidEntryPoint
 import io.chaldeaprjkt.gamespace.R
 import io.chaldeaprjkt.gamespace.data.AppSettings
+import io.chaldeaprjkt.gamespace.settings.SettingsActivity
 import io.chaldeaprjkt.gamespace.utils.ScreenUtils
 import io.chaldeaprjkt.gamespace.utils.dp
 import io.chaldeaprjkt.gamespace.utils.registerDraggableTouchListener
@@ -343,6 +344,10 @@ class GameBarService : Hilt_GameBarService() {
         val actionPanel = rootBarView.findViewById<ImageButton>(R.id.action_panel)
         actionPanel.setOnClickListener {
             showPanel = !showPanel
+        }
+        actionPanel.setOnLongClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+            true
         }
     }
 

Neoteric-OS/android_packages_apps_GameSpace

app/src/main/java/com/zeph/gamespace/gamebar/GameBarService.kt
@@ -41,6 +41,7 @@ import com.android.systemui.screenrecord.IRecordingCallback
 import dagger.hilt.android.AndroidEntryPoint
 import com.zeph.gamespace.R
 import com.zeph.gamespace.data.AppSettings
+import com.zeph.gamespace.settings.SettingsActivity
 import com.zeph.gamespace.utils.ScreenUtils
 import com.zeph.gamespace.utils.dp
 import com.zeph.gamespace.utils.registerDraggableTouchListener
@@ -340,6 +341,10 @@ class GameBarService : Hilt_GameBarService() {
         val actionPanel = rootBarView.findViewById<ImageButton>(R.id.action_panel)
         actionPanel.setOnClickListener {
             showPanel = !showPanel
+        }
+        actionPanel.setOnLongClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+            true
         }
     }
 